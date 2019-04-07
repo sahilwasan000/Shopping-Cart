c833fd1e70af4ad68446f0f17203944b23ffb95c
Sahil Wasan <sahilwasan000@gmail.com>
2019-04-07 23:00:49 +0530

    Init Commit

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var morgan = require('morgan');
 
 var app = express();
+
+//middleware
+app.use(morgan('dev'));
+
+app.get('/', (req,res) =>{
+  res.json('my name is sahil'); //res.send-> for sending a normal response
+});
 
 app.listen('8080', (err) => {
   if (err) throw err;
